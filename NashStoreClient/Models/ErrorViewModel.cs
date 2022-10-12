cec204bfcbcbc5e48e9987271b717af58b6e0f91
Kossmin <phamsn2001@gmail.com>
2022-10-12 11:39:17 +0700

    Add MVC Client

---
+++ NashStoreClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NashStoreClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
