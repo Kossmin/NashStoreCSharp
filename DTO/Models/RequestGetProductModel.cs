ae8bc81aa84ee98aa497a4b6b1a1a9eec750b595
Kossmin <phamsn2001@gmail.com>
2022-10-13 12:27:41 +0700

    Add DTO project

---
+++ DTO/Models/RequestGetProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DTO.Models
+{
+    public class RequestGetProductModel
+    {
+        public int PageIndex { get; set; }
+        public string ProductName { get; set; } = "";
+        public int CategoryId { get; set; } = 0;
+    }
+}
