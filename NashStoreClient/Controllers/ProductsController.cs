b49f45fa0f1f76e6bbf461b7b01ccb54a9db7ea1
Kossmin <phamsn2001@gmail.com>
2022-10-14 10:15:08 +0700

    Change how to controller get parameter

--- NashStoreClient/Controllers/ProductsController.cs
+++ NashStoreClient/Controllers/ProductsController.cs
@@ -19,7 +19,7 @@ namespace NashStoreClient.Controllers
         }
 
         // GET: Products
-        public async Task<IActionResult> Index(int pageIndex = 1)
+        public async Task<IActionResult> Index([FromQuery]int pageIndex = 1)
         {
             var productsList = await _productData.GetProducts(pageIndex);
             return View(productsList);
