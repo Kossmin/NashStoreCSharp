a670f532f06b40fcc76f289cfcebd34eeb87d495
Kossmin <phamsn2001@gmail.com>
2022-10-14 11:59:51 +0700

    Add category repo

---
+++ DAO/Implements/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObjects.Models;
+using DAO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Implements
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(NashStoreDbContext nashStoreDbContext) : base(nashStoreDbContext)
+        {
+        }
+    }
+}
