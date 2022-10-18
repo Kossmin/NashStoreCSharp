b100339497881cfd96f6a4a5999f5435dc0a658b
Kossmin <phamsn2001@gmail.com>
2022-10-18 17:34:54 +0700

    Add unit of work

--- NashStoreAPI/Controllers/RatingsController.cs
+++ NashStoreAPI/Controllers/RatingsController.cs
@@ -1,5 +1,6 @@
 ﻿using DAO.Interfaces;
 using DTO.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NashPhaseOne.DAO.Interfaces;
@@ -12,30 +13,32 @@ namespace NashStoreAPI.Controllers
     public class RatingsController : ControllerBase
     {
         private readonly IRatingRepository _ratingRepository;
-        private readonly IOrderDetailRepository _orderdetailRepository;
         private readonly IOrderRepository _orderRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public RatingsController(IRatingRepository ratingRepository, IOrderDetailRepository orderDetailRepository, IOrderRepository orderRepository)
+        public RatingsController(IRatingRepository ratingRepository, IOrderRepository orderRepository, IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _ratingRepository = ratingRepository;
-            _orderdetailRepository = orderDetailRepository;
             _orderRepository = orderRepository;
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> Create(RatingDTO model)
         {
             var userOrder = await _orderRepository.GetByAsync(o => o.UserId == model.CustomerId);
             var ifUserByThisProduct = userOrder?.OrderDetails.FirstOrDefault(od => od.ProductId == model.ProductId) != null;
             if (ifUserByThisProduct) 
             { 
-                await _ratingRepository.SaveAsync(new BusinessObjects.Models.Rating { ProductId = model.ProductId, UserId = model.CustomerId, Comment = model.Comment, Star = model.Star });
+                await _ratingRepository.SaveAsync(new BusinessObjects.Models.Rating { ProductId = model.ProductId, UserId = model.CustomerId, Comment = model.Comment, Star = BusinessObjects.Models.RatingStar.Neutral });
             }
             else
             {
                 return BadRequest(new { message = "You can't review the product that you haven't buy" });
             }
-            return NoContent();
+            await _unitOfWork.CommitAsync();
+            return Ok();
         }
     }
 }
