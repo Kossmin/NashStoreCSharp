9a2c91551270cd7c089f5971daf2e30abbe65cc4
Kossmin <phamsn2001@gmail.com>
2022-10-18 17:35:22 +0700

    Add OrderController

---
+++ NashStoreClient/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NashPhaseOne.DTO.Models.Order;
+using NashStoreClient.DataAccess;
+
+namespace NashPhaseOne.Client.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private IData _data;
+
+        public OrdersController(IData data)
+        {
+            _data = data;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Order([Bind("UserId, ProductId, Quantity")]OrderDTO order)
+        {
+            _data.CreateOrder(order);
+            return View();
+        }
+    }
+}
