66c9ff4c4bcaa2b30fe5995d406fb0ffbda8d508
Kossmin <phamsn2001@gmail.com>
2022-10-18 14:46:44 +0700

    Add OrderDetail repo

---
+++ DAO/Implements/OrderDetailRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Models;
+using DAO.Implements;
+using NashPhaseOne.DAO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DAO.Implements
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(NashStoreDbContext nashStoreDbContext) : base(nashStoreDbContext)
+        {
+        }
+    }
+}
---
+++ DAO/Interfaces/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects.Models;
+using DAO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DAO.Interfaces
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    {
+    }
+}
