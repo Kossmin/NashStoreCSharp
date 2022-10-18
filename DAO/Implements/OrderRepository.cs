e1178d81e3661b3df95ca8503cc2fb93e82fd20a
Kossmin <phamsn2001@gmail.com>
2022-10-18 14:47:03 +0700

    Add new repo (Rating, Order)

---
+++ DAO/Implements/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using BusinessObjects.Models;
+using DAO.Implements;
+using Microsoft.EntityFrameworkCore;
+using NashPhaseOne.DAO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DAO.Implements
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(NashStoreDbContext nashStoreDbContext) : base(nashStoreDbContext)
+        {
+        }
+
+        public virtual Task<Order> GetByAsync(Expression<Func<Order, bool>> expression)
+        {
+            return _nashStoreDbContext.Set<Order>().Include(o => o.OrderDetails).FirstOrDefaultAsync(expression);
+        }
+
+        public virtual IQueryable<Order> GetMany(Expression<Func<Order, bool>> expression)
+        {
+            return _nashStoreDbContext.Set<Order>().Include(o => o.OrderDetails).Where(expression);
+        }
+    }
+}
---
+++ DAO/Implements/RatingRepository.cs
@@ -0,0 +1,12 @@
+﻿using BusinessObjects.Models;
+using DAO.Interfaces;
+
+namespace DAO.Implements
+{
+    public class RatingRepository : Repository<Rating>, IRatingRepository
+    {
+        public RatingRepository(NashStoreDbContext nashStoreDbContext) : base(nashStoreDbContext)
+        {
+        }
+    }
+}
