f3c21caae7f27e4996e454edb1bfeea246f1db77
Kossmin <phamsn2001@gmail.com>
2022-10-19 18:30:33 +0700

    Add auto mapper profile

---
+++ NashStoreAPI/AutoMapperProfile/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BusinessObjects.Models;
+using NashPhaseOne.DTO.Models.Order;
+using NashPhaseOne.DTO.Models.Product;
+
+namespace NashPhaseOne.API.AutoMapperProfile
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<OrderDetail, OrderDetailDTO>();
+            CreateMap<Order, CartDTO>();
+            CreateMap<Product, ProductDetailDTO>();
+        }
+    }
+}
