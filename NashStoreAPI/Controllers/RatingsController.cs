2f499e87a29daa9148cac89edeb4869313ec5453
Kossmin <phamsn2001@gmail.com>
2022-10-19 18:32:04 +0700

    Add constraint for create rating function

--- NashStoreAPI/Controllers/RatingsController.cs
+++ NashStoreAPI/Controllers/RatingsController.cs
@@ -1,8 +1,10 @@
-﻿using DAO.Interfaces;
+﻿using BusinessObjects.Models;
+using DAO.Interfaces;
 using DTO.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NashPhaseOne.DAO.Interfaces;
 using NashPhaseOne.DTO.Models.Rating;
 
@@ -27,17 +29,28 @@ namespace NashStoreAPI.Controllers
         [Authorize]
         public async Task<IActionResult> Create(RatingDTO model)
         {
-            var userOrder = await _orderRepository.GetByAsync(o => o.UserId == model.CustomerId);
-            var ifUserByThisProduct = userOrder?.OrderDetails.FirstOrDefault(od => od.ProductId == model.ProductId) != null;
+            var userOrder = await _orderRepository.GetMany(o => o.UserId == model.CustomerId && o.Status != OrderStatus.Ordering).ToListAsync();
+            var userOrderDetails = new List<OrderDetail>();
+            foreach (var item in userOrder)
+            {
+                userOrderDetails.AddRange(item.OrderDetails);
+            }
+            var ifUserByThisProduct =userOrderDetails.FirstOrDefault(od => od.ProductId == model.ProductId) != null;
             if (ifUserByThisProduct) 
             { 
-                await _ratingRepository.SaveAsync(new BusinessObjects.Models.Rating { ProductId = model.ProductId, UserId = model.CustomerId, Comment = model.Comment, Star = BusinessObjects.Models.RatingStar.Neutral });
+                await _ratingRepository.SaveAsync(new BusinessObjects.Models.Rating { ProductId = model.ProductId, UserId = model.CustomerId, Comment = model.Comment, Star = model.Star });
             }
             else
             {
                 return BadRequest(new { message = "You can't review the product that you haven't buy" });
             }
-            await _unitOfWork.CommitAsync();
+            try
+            {
+                await _unitOfWork.CommitAsync();
+            }catch(Exception e)
+            {
+                return BadRequest(new { message = "You have reviewed this product already" });
+            }
             return Ok();
         }
     }
