38562c3c1b35f4d99e02d3f90b4edb53e5e51bc0
Kossmin <phamsn2001@gmail.com>
2022-10-20 17:24:43 +0700

    Allow products have multiple image URLs

--- BusinessObjects/Migrations/20221020083836_Product can have multiple images.Designer.cs
+++ BusinessObjects/Migrations/20221020083836_Product can have multiple images.Designer.cs
@@ -4,6 +4,7 @@ using BusinessObjects.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BusinessObjects.Migrations
 {
     [DbContext(typeof(NashStoreDbContext))]
-    partial class NashStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221020083836_Product can have multiple images")]
+    partial class Productcanhavemultipleimages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BusinessObjects/Migrations/20221020083836_Product can have multiple images.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    public partial class Productcanhavemultipleimages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgUrl",
+                table: "Products",
+                newName: "ImgUrls");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgUrls",
+                table: "Products",
+                newName: "ImgUrl");
+        }
+    }
+}
