c7666df7752ee43ad5e4ac026fbe8d53cdd10681
Kossmin <phamsn2001@gmail.com>
2022-10-20 17:26:04 +0700

    set defaul value for DTO

--- DTO/Models/Product/ProductDetailDTO.cs
+++ DTO/Models/Product/ProductDetailDTO.cs
@@ -9,8 +9,8 @@ namespace NashPhaseOne.DTO.Models.Product
     public class ProductDetailDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public decimal Price { get; set; }
-        public string ImgUrl { get; set; }
+        public string ImgUrl { get; set; } = "";
     }
 }
