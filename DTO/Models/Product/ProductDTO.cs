e755b41c27eb899b84ecb4d219712a26fec2d127
Kossmin <phamsn2001@gmail.com>
2022-10-23 21:53:18 +0700

    Change from object to props

--- DTO/Models/Product/ProductDTO.cs
+++ DTO/Models/Product/ProductDTO.cs
@@ -1,15 +1,23 @@
-﻿using BusinessObjects.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DTO.Models
+namespace NashPhaseOne.DTO.Models.Product
 {
     public class ProductDTO
     {
-        public Product Product { get; set; }
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ImportedDate { get; set; }
+        public List<string> ImgUrls { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public int Version { get; set; } = 1;
         public string CategoryName { get; set; }
     }
 }
