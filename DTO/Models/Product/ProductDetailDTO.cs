1d4e22e5e2ad152c08cfd524507c41ff78e1a7c8
Kossmin <phamsn2001@gmail.com>
2022-10-23 21:54:37 +0700

    Change type from string to list string

--- DTO/Models/Product/ProductDetailDTO.cs
+++ DTO/Models/Product/ProductDetailDTO.cs
@@ -11,6 +11,6 @@ namespace NashPhaseOne.DTO.Models.Product
         public int Id { get; set; }
         public string Name { get; set; } = "";
         public decimal Price { get; set; }
-        public string ImgUrl { get; set; } = "";
+        public List<string> ImgUrls { get; set; }
     }
 }
