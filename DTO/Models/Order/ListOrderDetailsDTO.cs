4f88b8ed7f33d897d2b08b12404076724cdcba81
Kossmin <phamsn2001@gmail.com>
2022-10-23 21:52:47 +0700

    Change name of class, add orderDate

--- DTO/Models/Order/ListOrderDetailsDTO.cs
+++ DTO/Models/Order/ListOrderDetailsDTO.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NashPhaseOne.DTO.Models.Order
 {
-    public class CartDTO
+    public class ListOrderDetailsDTO
     {
         public List<OrderDetailDTO> OrderDetails { get; set; } = new List<OrderDetailDTO>();
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
     }
 }
