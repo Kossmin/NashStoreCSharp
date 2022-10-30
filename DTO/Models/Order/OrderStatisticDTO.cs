d0f4041c49b4fc9f7d2e1d5095f29406390629b0
Kossmin <phamsn2001@gmail.com>
2022-10-30 22:54:03 +0700

    Add new DTO for admin site

---
+++ DTO/Models/Order/OrderStatisticDTO.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DTO.Models.Order
+{
+    public class OrderStatisticDTO
+    {
+        public string Type { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ DTO/Models/Product/AdminProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DTO.Models.Product
+{
+    public class AdminProductDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ImportedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public IEnumerable<IFormFile> Imgs { get; set; }
+    }
+}
