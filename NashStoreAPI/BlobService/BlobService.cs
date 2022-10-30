77ec428c90362a629d9ab7d35073b09ff96f15b4
Kossmin <phamsn2001@gmail.com>
2022-10-30 23:06:41 +0700

    This will handle the blob container

---
+++ NashStoreAPI/BlobService/BlobService.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using NashPhaseOne.API.BlobHelper;
+
+namespace NashPhaseOne.API.BlobService
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        const string CONTAINER_NAME = "nashstoreimage";
+
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public Task DeleteBlobAsync(string blobname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UploadFileBlobAsync(IFormFile file)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(CONTAINER_NAME);
+            var blobClient = containerClient.GetBlobClient(file.FileName);
+
+            await blobClient.UploadAsync(file.OpenReadStream(), new BlobHttpHeaders { ContentType = file.ContentType});
+        }
+    }
+}
---
+++ NashStoreAPI/BlobService/IBlobService.cs
@@ -0,0 +1,9 @@
+﻿namespace NashPhaseOne.API.BlobHelper
+{
+    public interface IBlobService
+    {
+        public Task UploadFileBlobAsync(IFormFile file);
+
+        public Task DeleteBlobAsync(string blobname);
+    }
+}
