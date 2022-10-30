b4c64eca1da7ecb2052e7bbd1e0f62a2bb57ecae
Kossmin <phamsn2001@gmail.com>
2022-10-30 23:06:24 +0700

    Add cargorydto, product mapping profile

--- NashStoreAPI/AutoMapperProfile/MapperProfile.cs
+++ NashStoreAPI/AutoMapperProfile/MapperProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BusinessObjects.Models;
+using DTO.Models.Category;
 using NashPhaseOne.DTO.Models.Order;
 using NashPhaseOne.DTO.Models.Product;
 using NashPhaseOne.DTO.Models.Rating;
@@ -17,6 +18,8 @@ namespace NashPhaseOne.API.AutoMapperProfile
             CreateMap<Product, ProductDTO>()
                 .ForMember(dest => dest.CategoryName, opt=> opt.MapFrom(src=>src.Category.Name))
                 .ForMember(dest => dest.ImgUrls, opt => opt.MapFrom(src => src.ImgUrls));
+            CreateMap<Category, CategoryDTO>();
+            CreateMap<AdminProductDTO, Product>();
         }
     }
 }
