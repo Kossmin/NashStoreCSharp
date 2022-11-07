35c9a32eeeeda9198631db4aa51d547191b6f2bc
Kossmin <phamsn2001@gmail.com>
2022-11-07 16:09:46 +0700

    Add cate funcs (Add, toggle, update, getall)

--- NashStoreAPI/Controllers/CategoriesController.cs
+++ NashStoreAPI/Controllers/CategoriesController.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using BusinessObjects.Models;
+using NashPhaseOne.BusinessObjects.Models;
 using DTO.Models;
 using DTO.Models.Category;
 using DAO.Interfaces;
 using AutoMapper;
+using NashPhaseOne.DTO.Models;
+using Microsoft.AspNetCore.Authorization;
+using NashPhaseOne.API.Filters;
 
 namespace NashStoreAPI.Controllers
 {
@@ -18,11 +21,15 @@ namespace NashStoreAPI.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryRepository _categoryRepository;
+        private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CategoriesController(ICategoryRepository categoryRepository, IMapper mapper)
+        public CategoriesController(ICategoryRepository categoryRepository, IMapper mapper, IUnitOfWork unitOfWork, IProductRepository productRepository)
         {
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _productRepository = productRepository;
             _categoryRepository = categoryRepository;
         }
 
@@ -30,87 +37,80 @@ namespace NashStoreAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategories()
         {
-            var cateResponse = await _categoryRepository.GetAll().ToListAsync();
+            var cateResponse = await _categoryRepository.GetAll().Where(x=> !x.IsDeleted).ToListAsync();
             var categories = _mapper.Map<List<CategoryDTO>>(cateResponse);
             
             return categories;
         }
 
-        //// GET: api/Categories/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Category>> GetCategory(int id)
-        //{
-        //    var category = await _categoryRepository.Categories.FindAsync(id);
+        [HttpGet("available")]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetAllCategories()
+        {
+            var cateResponse = await _categoryRepository.GetAll().ToListAsync();
+            var categories = _mapper.Map<List<CategoryDTO>>(cateResponse);
 
-        //    if (category == null)
-        //    {
-        //        return NotFound();
-        //    }
+            return categories;
+        }
 
-        //    return category;
-        //}
+        [HttpPut("toggle")]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult> ToggleStatus([FromBody] IdString id)
+        {
+            if (string.IsNullOrEmpty(id.Id))
+            {
+                return BadRequest(new { message = "Can't find" });
+            }
+            var cate = await _categoryRepository.GetByAsync(c => c.Id == int.Parse(id.Id));
+            cate.IsDeleted = !cate.IsDeleted;
+            if (cate.IsDeleted)
+            {
+                cate.Products.ForEach(x =>
+                {
+                    x.IsDeleted = true;
+                });
+            }
+            await _categoryRepository.UpdateAsync(cate);
+            await _unitOfWork.CommitAsync();
+            return Ok();
+        }
 
-        //// PUT: api/Categories/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutCategory(int id, Category category)
-        //{
-        //    if (id != category.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult> Add([FromBody] CategoryDTO category)
+        {
+            try
+            {
+                var cate = _mapper.Map<Category>(category);
+                await _categoryRepository.SaveAsync(cate);
+                await _unitOfWork.CommitAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+        }
 
-        //    _categoryRepository.Entry(category).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _categoryRepository.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!CategoryExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //// POST: api/Categories
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<Category>> PostCategory(Category category)
-        //{
-        //    _categoryRepository.Categories.Add(category);
-        //    await _categoryRepository.SaveChangesAsync();
-
-        //    return CreatedAtAction("GetCategory", new { id = category.Id }, category);
-        //}
-
-        //// DELETE: api/Categories/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteCategory(int id)
-        //{
-        //    var category = await _categoryRepository.Categories.FindAsync(id);
-        //    if (category == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _categoryRepository.Categories.Remove(category);
-        //    await _categoryRepository.SaveChangesAsync();
-
-        //    return NoContent();
-        //}
-
-        //private bool CategoryExists(int id)
-        //{
-        //    return _categoryRepository.Categories.Any(e => e.Id == id);
-        //}
+        [HttpPatch]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult> Update([FromBody] CategoryDTO category)
+        {
+            try
+            {
+                var cate = _mapper.Map<Category>(category);
+                await _categoryRepository.UpdateAsync(cate);
+                await _unitOfWork.CommitAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+        }
     }
 }
