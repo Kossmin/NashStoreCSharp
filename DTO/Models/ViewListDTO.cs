82aa42e88a8aa4019d9386a442583655e7e80cb5
Kossmin <phamsn2001@gmail.com>
2022-11-07 16:05:44 +0700

    Change name using namespace

--- DTO/Models/ViewListDTO.cs
+++ DTO/Models/ViewListDTO.cs
@@ -9,7 +9,7 @@ namespace DTO.Models
     public class ViewListDTO<T> where T : class
     {
         public List<T> ModelDatas { get; set; } = new List<T>();
-        public int MaxPage { get; set; }
-        public int PageIndex { get; set; }
+        public int MaxPage { get; set; } = 0;
+        public int PageIndex { get; set; } = 0;
     }
 }
