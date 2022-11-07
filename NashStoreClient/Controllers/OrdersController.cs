3a8a72ccdd2ab7c52825799aa8f3bd852e95e69f
Kossmin <phamsn2001@gmail.com>
2022-11-07 16:20:58 +0700

    authorize

--- NashStoreClient/Controllers/OrdersController.cs
+++ NashStoreClient/Controllers/OrdersController.cs
@@ -15,22 +15,32 @@ namespace NashPhaseOne.Client.Controllers
             _data = data;
         }
 
+        [Authorize]
         public async Task<ActionResult> Cart()
         {
             var userId = User.Claims.FirstOrDefault(u => u.Type == "userid").Value;
             var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
-            var cartDto = await _data.GetCartAsync(new IdString { Id = userId}, token);
-            if(cartDto.OrderDetails.Count() == 0)
+            try
             {
-                ViewData["cartDto"] = null;
+                var cartDto = await _data.GetCartAsync(new IdString { Id = userId }, token);
+                if (cartDto.OrderDetails.Count() == 0)
+                {
+                    ViewData["cartDto"] = null;
+                }
+                else
+                {
+                    ViewData["cartDto"] = cartDto;
+                }
+                return View(cartDto);
             }
-            else
+            catch (Refit.ApiException e)
             {
-                ViewData["cartDto"] = cartDto;
+                TempData["Error"] = "Out of session";
+                return RedirectToAction("Login", "Auth");
             }
-            return View(cartDto);
         }
 
+        [Authorize]
         public async Task<ActionResult> CancelOrder(int id)
         {
             var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
@@ -44,55 +54,106 @@ namespace NashPhaseOne.Client.Controllers
 
             }
 
-            return RedirectToAction("PaidOrders");
+            return RedirectToAction("Index", "Products");
         }
 
+        [Authorize]
         public async Task<ActionResult> CanceledOrders()
         {
             var userId = User.Claims.FirstOrDefault(u => u.Type == "userid").Value;
             var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
-            var orders = await _data.GetCanceledOrdersAsync(new IdString { Id = userId}, token);
-            if (orders.Count() == 0)
+            try
             {
-                ViewData["cartDto"] = null;
+                var orders = await _data.GetCanceledOrdersAsync(new IdString { Id = userId }, token);
+                if (orders.Count() == 0)
+                {
+                    ViewData["cartDto"] = null;
+                }
+                else
+                {
+                    ViewData["cartDto"] = orders;
+                }
+                return View(orders);
             }
-            else
+            catch (Refit.ApiException e)
             {
-                ViewData["cartDto"] = orders;
+                TempData["Error"] = "Out of session";
+                return RedirectToAction("Login", "Auth");
             }
-            return View(orders);
         }
 
+        [Authorize]
         public async Task<ActionResult> PaidOrders()
         {
             var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
             var userId = User.Claims.FirstOrDefault(u => u.Type == "userid").Value;
-            var orders = await _data.GetPaidOrdersAsync(new IdString { Id = userId }, token);
-            if (orders.Count() == 0)
-            {
-                ViewData["cartDto"] = null;
+            try{
+                var orders = await _data.GetDoneOrdersAsync(new IdString { Id = userId }, token);
+                if (orders.Count() == 0)
+                {
+                    ViewData["cartDto"] = null;
+                }
+                else
+                {
+                    ViewData["cartDto"] = orders;
+                }
+                return View(orders);
             }
-            else
+            catch (Refit.ApiException e)
             {
-                ViewData["cartDto"] = orders;
+                TempData["Error"] = "Out of session";
+                return RedirectToAction("Login", "Auth");
             }
-            return View(orders);
-        }
+}
 
+        [Authorize]
         public async Task<ActionResult> DeliveringOrders()
         {
             var userId = User.Claims.FirstOrDefault(u => u.Type == "userid").Value;
             var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
-            var orders = await _data.GetDeliveringOrdersAsync(new IdString { Id = userId }, token);
-            if (orders.Count() == 0)
-            {
-                ViewData["cartDto"] = null;
+            try { 
+                var orders = await _data.GetDeliveringOrdersAsync(new IdString { Id = userId }, token);
+                if (orders.Count() == 0)
+                {
+                    ViewData["cartDto"] = null;
+                }
+                else
+                {
+                    ViewData["cartDto"] = orders;
+                }
+                return View(orders);
             }
-            else
+            catch (Refit.ApiException e)
             {
-                ViewData["cartDto"] = orders;
+                TempData["Error"] = "Out of session";
+                return RedirectToAction("Login", "Auth");
             }
-            return View(orders);
+
+        }
+
+        [Authorize]
+        public async Task<ActionResult> PendingOrders()
+        {
+            var userId = User.Claims.FirstOrDefault(u => u.Type == "userid").Value;
+            var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
+            try
+            {
+                var orders = await _data.GetPendingOrdersAsync(new IdString { Id = userId }, token);
+                if (orders.Count() == 0)
+                {
+                    ViewData["cartDto"] = null;
+                }
+                else
+                {
+                    ViewData["cartDto"] = orders;
+                }
+                return View(orders);
+            }
+            catch (Refit.ApiException e)
+            {
+                TempData["Error"] = "Out of session";
+                return RedirectToAction("Login", "Auth");
+            }
         }
 
         [Authorize]
@@ -104,12 +165,14 @@ namespace NashPhaseOne.Client.Controllers
             return RedirectToAction("Index", "Products");
         }
 
+        [Authorize]
         public async Task<ActionResult> Checkout()
         {
             var userId = User.Claims.FirstOrDefault(u => u.Type == "userid").Value;
+            var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
             try
             {
-                await _data.CheckoutAsync(new IdString { Id = userId });
+                await _data.CheckoutAsync(new IdString { Id = userId }, token);
             }
             catch (Refit.ApiException e)
             {
@@ -118,28 +181,33 @@ namespace NashPhaseOne.Client.Controllers
                 TempData["Error"] = errorList?.FirstOrDefault(x=> x.Key == "message").Value;
                 
             }
+            TempData["Message"] = "Order success";
             return RedirectToAction("Index", "Products");
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<ActionResult> Edit(string OrderDetailId, int Quantity)
         {
             try
             {
+                var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
                 var newOrderDetail = new OrderDetailDTO { Id = int.Parse(OrderDetailId), Quantity = Quantity, Price = 0, Product = new DTO.Models.Product.ProductDetailDTO() };
-                await _data.UpdateOrderDetailAsync(newOrderDetail);
+                await _data.UpdateOrderDetailAsync(newOrderDetail, token);
+                TempData["Message"] = "Edit success";
             }catch(Refit.ApiException e)
             {
                 var errorList = await e.GetContentAsAsync<Dictionary<string, string>>();
-                TempData["Error"] = errorList?.First().Value; ;
+                TempData["Error"] = errorList?.First().Value;
             }
-            TempData["Message"] = "Edit success";
             return RedirectToAction("Cart");
         }
 
+        [Authorize]
         public async Task<ActionResult> DeleteOrderDetail(int id)
         {
-            await _data.DeleteOrderDetailAsync(id);
+            var token = User.Claims.FirstOrDefault(u => u.Type == "token").Value;
+            await _data.DeleteOrderDetailAsync(id, token);
             TempData["Message"] = "Remove success";
             return RedirectToAction("Cart");
         }
