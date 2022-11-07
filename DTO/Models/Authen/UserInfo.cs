5d0202a22ee68c2f456fc94611a9c7c3eef12a56
Kossmin <phamsn2001@gmail.com>
2022-11-07 16:00:32 +0700

    new DTOs

---
+++ DTO/Models/Authen/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DTO.Models.Authen
+{
+    public class UserInfo
+    {
+        public string Email { get; set; }
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public bool IsBanned { get; set; }
+    }
+}
---
+++ DTO/Models/Statistic/MonthlyIncomeStatisticDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DTO.Models.Statistic
+{
+    public class MonthlyIncomeStatisticDTO
+    {
+        public Dictionary<int, decimal> MonthlyIncome { get; set; }
+    }
+}
