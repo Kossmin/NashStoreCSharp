0835efc72f8b3db36eb3fa08c0c46bb9cb7e8c8b
Kossmin <phamsn2001@gmail.com>
2022-11-07 15:59:24 +0700

    override getby function

--- DAO/Implements/CategoryRepository.cs
+++ DAO/Implements/CategoryRepository.cs
@@ -1,8 +1,10 @@
-﻿using BusinessObjects.Models;
-using DAO.Interfaces;
+﻿using DAO.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using NashPhaseOne.BusinessObjects.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,11 @@ namespace DAO.Implements
     {
         public CategoryRepository(NashStoreDbContext nashStoreDbContext) : base(nashStoreDbContext)
         {
+        }
+
+        public override async Task<Category> GetByAsync(Expression<Func<Category, bool>> expression)
+        {
+            return await _nashStoreDbContext.Categories.Include(x => x.Products).FirstOrDefaultAsync(expression);
         }
     }
 }
