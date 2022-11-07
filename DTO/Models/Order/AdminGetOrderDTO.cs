f50128d6ce96b9aaeaffc9b3cb0d253c0ffd32f9
Kossmin <phamsn2001@gmail.com>
2022-11-07 16:01:07 +0700

    New admin DTOs

---
+++ DTO/Models/Order/AdminGetOrderDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashPhaseOne.DTO.Models.Order
+{
+    public class AdminGetOrderDTO
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public string Status { get; set; }
+
+        public List<OrderDetailDTO> OrderDetails { get; set; }
+    }
+}
