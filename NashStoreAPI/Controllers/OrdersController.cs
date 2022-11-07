4f7dfc409abbab0377849a6b4c1b9bac31a811f2
Kossmin <phamsn2001@gmail.com>
2022-11-07 16:16:59 +0700

    Add authorize, new funcs(get all, get by status, monthly statistic, confirm, get pending order,

--- NashStoreAPI/Controllers/OrdersController.cs
+++ NashStoreAPI/Controllers/OrdersController.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using BusinessObjects.Models;
+using NashPhaseOne.BusinessObjects.Models;
 using DAO.Interfaces;
 using DTO.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -9,12 +9,13 @@ using Microsoft.EntityFrameworkCore;
 using NashPhaseOne.DAO.Interfaces;
 using NashPhaseOne.DTO.Models;
 using NashPhaseOne.DTO.Models.Order;
+using NashPhaseOne.API.Filters;
+using NashPhaseOne.DTO.Models.Statistic;
 
 namespace NashPhaseOne.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
     public class OrdersController : ControllerBase
     {
         private IOrderRepository _orderRepository;
@@ -33,30 +34,89 @@ namespace NashPhaseOne.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ViewListDTO<Order>> GetAll([FromQuery]int pageIndex = 1)
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult<ViewListDTO<AdminGetOrderDTO>>> GetAll([FromQuery]int pageIndex = 1)
         {
-            return await _orderRepository.PagingAsync(_orderRepository.GetAll().Where(x=>x.Status != OrderStatus.Ordering), pageIndex);
+            var orders = await _orderRepository.PagingAsync(_orderRepository.GetMany(x => x.Status != OrderStatus.Ordering).OrderBy(x => x.Status != OrderStatus.Pending).ThenBy(x=>x.OrderDate), pageIndex);
+            if (orders == null)
+            {
+                return new ViewListDTO<AdminGetOrderDTO>();
+            }
+            var castOrders = _mapper.Map<List<AdminGetOrderDTO>>(orders.ModelDatas);
+            var result = new ViewListDTO<AdminGetOrderDTO> { ModelDatas = castOrders, MaxPage= orders.MaxPage, PageIndex = orders.PageIndex};
+            return result;
+        }
+
+        [HttpGet("getbystatus")]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult<ViewListDTO<AdminGetOrderDTO>>> GetByStatus([FromQuery]int status, int pageIndex = 1)
+        {
+            var orders = await _orderRepository.PagingAsync(_orderRepository.GetAll().Where(x => x.Status == (OrderStatus)status), pageIndex);
+            if(orders == null)
+            {
+                return new ViewListDTO<AdminGetOrderDTO>();
+            }
+            var castOrders = _mapper.Map<List<AdminGetOrderDTO>>(orders.ModelDatas);
+            var result = new ViewListDTO<AdminGetOrderDTO> { ModelDatas = castOrders, MaxPage = orders.MaxPage, PageIndex = orders.PageIndex };
+            return result;
         }
 
         [HttpGet("getorderstatistic")]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
         public async Task<List<OrderStatisticDTO>> GetOrderStatistics()
         {
-            var orders = await _orderRepository.GetAll().Where(x=>x.Status != OrderStatus.Ordering).ToListAsync();
-            var paidOrders = orders.Where(x => x.Status == OrderStatus.Paid);
+            var orders = await _orderRepository.GetMany(x=>x.Status != OrderStatus.Ordering).ToListAsync();
+            var paidOrders = orders.Where(x => x.Status == OrderStatus.Done);
             var delivering = orders.Where(x => x.Status == OrderStatus.Delivering);
             var delivered = orders.Where(x => x.Status == OrderStatus.Delivered);
             var pending = orders.Where(x => x.Status == OrderStatus.Pending);
             var canceled = orders.Where(x => x.Status == OrderStatus.Canceled);
             return new List<OrderStatisticDTO> {
-                new OrderStatisticDTO { Type = OrderStatus.Paid.ToString(), Amount = paidOrders.Count() },
+                new OrderStatisticDTO { Type = OrderStatus.Done.ToString(), Amount = paidOrders.Count() },
                 new OrderStatisticDTO { Type = OrderStatus.Delivering.ToString(), Amount = delivering.Count() },
                 new OrderStatisticDTO { Type = OrderStatus.Delivered.ToString(), Amount = delivered.Count() },
                 new OrderStatisticDTO { Type = OrderStatus.Pending.ToString(), Amount = pending.Count() },
                 new OrderStatisticDTO { Type = OrderStatus.Canceled.ToString(), Amount = canceled.Count()},};
         }
 
+        [HttpGet("getmonthlystatistic")]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<MonthlyIncomeStatisticDTO> GetMonthlyIncomeStatistics()
+        {
+            var orders = _orderRepository.GetMany(x => x.Status != OrderStatus.Ordering && x.OrderDate.Year == DateTime.Now.Year);
+            var months = new MonthlyIncomeStatisticDTO { MonthlyIncome = new Dictionary<int, decimal> { 
+                { 1, 0 },
+                { 2, 0 },
+                { 3, 0 },
+                { 4, 0 },
+                { 5, 0 },
+                { 6, 0 },
+                { 7, 0 },
+                { 8, 0 },
+                { 9, 0 },
+                { 10, 0 },
+                { 11, 0 },
+                { 12, 0},} 
+            };
+            foreach (var item in orders)
+            {
+                var monthIncome = months.MonthlyIncome.FirstOrDefault(x => x.Key == item.OrderDate.Month).Value;
+                foreach (var orderDetail in item.OrderDetails)
+                {
+                    monthIncome += orderDetail.Price * orderDetail.Quantity;
+                }
+                months.MonthlyIncome[item.OrderDate.Month] = monthIncome;
+            }
+            return months;
+        }
+
         [HttpPost("cart")]
         [Authorize]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
         public async Task<ListOrderDetailsDTO> GetCart([FromBody]IdString userId)
         {
             var order = await _orderRepository.GetByAsync(o => o.Status == OrderStatus.Ordering && o.UserId == userId.Id);
@@ -72,11 +132,31 @@ namespace NashPhaseOne.API.Controllers
             }
         }
 
+        [HttpPatch("confirm")]
+        [Authorize(Roles = "Admin")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult> Confirm([FromBody] IdString id)
+        {
+            var order = await _orderRepository.GetByAsync(o => o.Id == int.Parse(id.Id) && o.Status == OrderStatus.Pending);
+            if (order == null)
+            {
+                return BadRequest(new { message = "Can't find this order" });
+            }
+            else
+            {
+                order.Status = OrderStatus.Delivering;
+                await _orderRepository.UpdateAsync(order);
+            }
+            await _unitOfWork.CommitAsync();
+            return Ok();
+        }
+
         [HttpPut("cancel")]
         [Authorize]
-        public async Task<ActionResult> CancelOrders([FromBody] IdString id)
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<ActionResult> CancelOrder([FromBody] IdString id)
         {
-            var order = await _orderRepository.GetByAsync(o => o.Id == int.Parse(id.Id));
+            var order = await _orderRepository.GetByAsync(o => o.Id == int.Parse(id.Id) && o.Status == OrderStatus.Pending);
 
             if (order == null)
             {
@@ -111,7 +191,8 @@ namespace NashPhaseOne.API.Controllers
 
         [HttpPost("canceled")]
         [Authorize]
-        public async Task<List<ListOrderDetailsDTO>> GetCanceledOrders([FromBody] IdString userId)
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<List<ListOrderDetailsDTO>> GetCanceledOrder([FromBody] IdString userId)
         {
             var order = _orderRepository.GetMany(o => o.Status == OrderStatus.Canceled && o.UserId == userId.Id).Include(x => x.OrderDetails).ThenInclude(x => x.Product).ToList();
 
@@ -126,11 +207,30 @@ namespace NashPhaseOne.API.Controllers
             }
         }
 
-        [HttpPost("paid")]
+        [HttpPost("pending")]
         [Authorize]
-        public async Task<List<ListOrderDetailsDTO>> GetPaidOrders([FromBody] IdString userId)
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<List<ListOrderDetailsDTO>> GetPendingOrder([FromBody] IdString userId)
         {
-            var order = _orderRepository.GetMany(o => o.Status == OrderStatus.Paid && o.UserId == userId.Id).Include(x=>x.OrderDetails).ThenInclude(x=>x.Product).ToList();
+            var order = _orderRepository.GetMany(o => o.Status == OrderStatus.Pending && o.UserId == userId.Id).Include(x => x.OrderDetails).ThenInclude(x => x.Product).ToList();
+
+            if (order != null)
+            {
+                var mappedOrder = _mapper.Map<List<Order>, List<ListOrderDetailsDTO>>(order);
+                return mappedOrder;
+            }
+            else
+            {
+                return new List<ListOrderDetailsDTO>();
+            }
+        }
+
+        [HttpPost("done")]
+        [Authorize]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<List<ListOrderDetailsDTO>> GetDoneOrder([FromBody] IdString userId)
+        {
+            var order = _orderRepository.GetMany(o => o.Status == OrderStatus.Done && o.UserId == userId.Id).Include(x=>x.OrderDetails).ThenInclude(x=>x.Product).ToList();
 
             if (order != null)
             {
@@ -145,9 +245,10 @@ namespace NashPhaseOne.API.Controllers
 
         [HttpPost("delivering")]
         [Authorize]
-        public async Task<List<ListOrderDetailsDTO>> GetDeliveringOrders([FromBody] IdString userId)
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
+        public async Task<List<ListOrderDetailsDTO>> GetDeliveringOrder([FromBody] IdString userId)
         {
-            var order = _orderRepository.GetMany(o => o.Status == OrderStatus.Delivering && o.UserId == userId.Id).ToList();
+            var order = _orderRepository.GetMany(o => o.Status == OrderStatus.Delivering && o.UserId == userId.Id).Include(x => x.OrderDetails).ThenInclude(x => x.Product).ToList();
 
             if (order != null)
             {
@@ -161,7 +262,8 @@ namespace NashPhaseOne.API.Controllers
         }
 
         [HttpPost("checkout")]
-        //[AllowAnonymous]
+        [Authorize(Roles = "Customer")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
         public async Task<ActionResult> Checkout([FromBody]IdString userId)
         {
             var order = await _orderRepository.GetByAsync(o => o.Status == OrderStatus.Ordering && o.UserId == userId.Id);
@@ -196,7 +298,7 @@ namespace NashPhaseOne.API.Controllers
                 }            
             }
 
-            order.Status = OrderStatus.Paid;
+            order.Status = OrderStatus.Pending;
             order.OrderDate = DateTime.UtcNow;
             await _orderRepository.UpdateAsync(order);
             await _unitOfWork.CommitAsync();
@@ -204,7 +306,8 @@ namespace NashPhaseOne.API.Controllers
         }
 
         [HttpPost("create")]
-        [Authorize]
+        [Authorize(Roles = "Customer")]
+        [TypeFilter(typeof(CustomAuthorizeFilter))]
         public async Task Create(OrderDTO order)
         {
             var availableOrder = await _orderRepository.GetByAsync(o => o.UserId == order.UserId && o.Status == BusinessObjects.Models.OrderStatus.Ordering);
